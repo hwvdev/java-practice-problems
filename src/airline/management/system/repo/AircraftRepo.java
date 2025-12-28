0802d0e2d82f9609eb2bd588d1f9512a7365ab18
vijay <hwvdev@gmail.com>
2025-12-28 17:59:40 +0530

    initial commit

--- src/airline/management/system/repo/AircraftRepo.java
+++ src/airline/management/system/repo/AircraftRepo.java
@@ -7,7 +7,15 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AircraftRepo {
+    private final static AircraftRepo AIRCRAFT_INSTANCE = new AircraftRepo();
+
     private final Map<String, Aircraft> registeredAircraft = new ConcurrentHashMap<>();
+
+    private AircraftRepo() {}
+
+    public static AircraftRepo getAircraftInstance() {
+        return AIRCRAFT_INSTANCE;
+    }
 
     public void addAircraft(Aircraft aircraft) {
         registeredAircraft.put(aircraft.getAircraftId(), aircraft);
